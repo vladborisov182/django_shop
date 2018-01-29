eb3d6d5b2e07ea0936e508671b65efc4dbe5570d
Vladislav Borisov <33347052+vladborisov182@users.noreply.github.com>
2018-01-29 21:39:01 +0200

    Update views.py

--- wishlist/views.py
+++ wishlist/views.py
@@ -7,7 +7,6 @@ from wishlist.forms import WishlistForm
 from django.contrib.auth.decorators import login_required
 
 
-
 @login_required(login_url='/accounts/login/')
 def wishlist_add(request, product_id):
     user = auth.get_user(request)
